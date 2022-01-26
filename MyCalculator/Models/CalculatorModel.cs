3384677a52bc19592f2332f82944be57f1129fa1
Dmitriy Larionov <larionov.d.y@gmail.com>
2022-01-26 13:47:34 +0300

    2

--- MyCalculator/Models/CalculatorModel.cs
+++ MyCalculator/Models/CalculatorModel.cs
@@ -8,49 +8,141 @@ namespace MyCalculator.Models
 {
     public class CalculatorModel
     {
-        private string number1;
+        private string result;
 
-        private string number2;
+        public string FirstOperand { get; set; }
 
-        private char operation;
+        public string SecondOperand { get; set; }
 
-        private double result;
+        public string Operation { get; set; }
 
-        private string display;
-
-        public string Display
-        {
-            get { return display; }
-            set { display = value; }
-        }
-
-        public string Number1
-        {
-            get { return number1; }
-            set { number1 = value; }
-        }
-
-        public string Number2
-        {
-            get { return number2; }
-            set { number2 = value; }
-        }
-
-        public char Operation
-        {
-            get { return operation; }
-            set { operation = value; }
-        }
-
-        public double Result
-        {
-            get { return result; }
-            set { result = value; }
-        }
+        public string Result { get => result; }
 
         public CalculatorModel()
         {
-            //Clear();
+            FirstOperand = string.Empty;
+            SecondOperand = string.Empty;
+            Operation = string.Empty;
+            result = string.Empty;
+        }
+
+        public CalculatorModel(string firstOperand, string operation) 
+        {
+            ValidateOperand(firstOperand);            
+            ValidateOperation(operation);
+
+            FirstOperand = firstOperand;
+            SecondOperand = string.Empty;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public CalculatorModel(string firstOperand, string secondOperand, string operation)
+        {
+            ValidateOperand(firstOperand);
+            ValidateOperand(secondOperand);
+            ValidateOperation(operation);
+
+            FirstOperand = firstOperand;
+            SecondOperand = secondOperand;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public void CalculateResult()
+        {
+            ValidateData();
+            try
+            {
+                switch (Operation)
+                {
+                    case ("+"):
+                        result = (Convert.ToDouble(FirstOperand) + Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("-"):
+                        result = (Convert.ToDouble(FirstOperand) - Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("*"):
+                        result = (Convert.ToDouble(FirstOperand) * Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("/"):
+                        result = (Convert.ToDouble(FirstOperand) / Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("1/x"):
+                        result = (1 / (Convert.ToDouble(FirstOperand))).ToString();
+                        break;
+
+                    case ("sqr"):
+                        result = Math.Pow(Convert.ToDouble(FirstOperand), 2).ToString();
+                        break;
+
+                    case ("sqrt"):
+                        result = Math.Sqrt(Convert.ToDouble(FirstOperand)).ToString();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                result = "ERROR";
+                throw;
+            }
+        }
+
+        private void ValidateOperand(string operand)
+        {
+            try
+            {
+                Convert.ToDouble(operand);
+            }
+            catch (Exception)
+            {
+                result = "ERROR";
+                throw;
+            }
+        }
+
+        private void ValidateOperation(string operation)
+        {
+            switch (operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                case "1/x":
+                case "sqr":
+                case "sqrt":
+                    break;
+                default:
+                    result = "ERROR";
+                    throw new ArgumentException("Unknown Operation: " + operation, "operation");
+            }
+        }
+
+        private void ValidateData()
+        {
+            switch (Operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                    ValidateOperand(FirstOperand);
+                    ValidateOperand(SecondOperand);
+                    break;
+                case "1/x":
+                case "sqr":
+                case "sqrt":
+                    ValidateOperand(FirstOperand);
+                    break;
+                default:
+                    result = "ERROR ";
+                    throw new ArgumentException("Unknown Operation: " + Operation, "operation");
+            }
         }
     }
 }
---
+++ MyCalculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+    }
+}
